3b635ae7c0861a336a01c538f3ef17dfddec081f
arnaud <arnaudthomas41@live.fr>
2023-01-24 14:23:13 +0100

    Add on test

---
+++ src/main/java/org/example/Exemple.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Exemple {
+
+    private int number;
+
+    public Exemple(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
